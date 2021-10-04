7983634cdb49c71870e34e8a0c62055c0a41a0e5
Chrispasma <chris.pasma@student.nhlstenden.com>
2021-10-04 12:05:44 +0200

    voorbeeld game

---
+++ voorbeelden game p1/space battle game/space battle game/Game.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+
+namespace space_battle_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /* <Button x:Name="Pause" Height="50" Width="60" Background="Yellow" Canvas.Left="240" Canvas.Top="10" RenderTransformOrigin="-1.12,0.948"></Button>*/
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer Gametimer = new DispatcherTimer();
+        bool MoveLeft, MoveRight;
+        List<Rectangle> ItemRemover = new List<Rectangle>();
+
+        Random Ran = new Random();
+
+        int EnemySpriteCounter = 0;
+        int EnemyCounter = 100;
+        int PlayerSpeed = 10;
+        int Limit = 50;
+        int Score = 0;
+        int Damage = 0;
+        int EnemySpeed = 10;
+        bool PauseOnOff = true;
+        
+        Rect PlayerHitBox;
+
+
+        public MainWindow()
+        {
+
+            Gametimer.Interval = TimeSpan.FromMilliseconds(20);
+            Gametimer.Tick += Gameloop;
+            Gametimer.Start();
+
+            MyCanvas.Focus();
+            //achtergrond//
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect( 0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            MyCanvas.Background = bg;
+
+            //player foto//
+            ImageBrush PlayerImage = new ImageBrush();
+            PlayerImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/player.png"));
+            Player.Fill = PlayerImage;
+
+
+        }
+        //pauzeknop//
+        private void WhenButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            if (PauseOnOff == true)
+            {
+                
+                Gametimer.Stop();
+               
+
+            }
+            if (PauseOnOff == false)
+            {
+               
+                Gametimer.Start();
+               
+
+            }
+            if (PauseOnOff == true)
+            {
+                PauseOnOff = false;
+            }
+            else
+            {
+                PauseOnOff = true;
+            }
+        }
+
+        private void Gameloop(object sender, EventArgs e)
+        {
+            PlayerHitBox = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
+
+            EnemyCounter -= 1;
+            //score setting//
+            Scoretext.Content = "score: " + Score;
+            Damagetext.Content = "Damage: " + Damage;
+            //enemy spawning//
+            if (EnemyCounter < 0)
+            {
+                MakeEnimies();
+                EnemyCounter = Limit; 
+            }
+            //player movement//
+            if (MoveLeft == true && Canvas.GetLeft(Player) > 0)
+            {
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) - PlayerSpeed);
+            }
+            if (MoveRight == true && Canvas.GetLeft(Player) + 90  < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) + PlayerSpeed);
+            }
+            //actual gameplay//
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {   //bullet hit op enemy//
+                if (x is Rectangle && (string)x.Tag == "bullet" )
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) - 20);
+
+                    Rect BulletHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+                    if (Canvas.GetTop(x) <10 )
+                    {
+                        ItemRemover.Add(x);
+                    }
+
+                    foreach (var y in MyCanvas.Children.OfType<Rectangle>())
+                    {
+                        if (y is Rectangle && (string)y.Tag == "enemy")
+                        {
+                            Rect EnemyHit = new Rect(Canvas.GetLeft(y), Canvas.GetTop(y), y.Width, y.Height);
+
+                            if (BulletHitBox.IntersectsWith(EnemyHit))
+                            {
+                                ItemRemover.Add(x);
+                                ItemRemover.Add(y);
+                                Score++;
+                            }
+                        }
+                    }
+                }
+
+                if (x is Rectangle && (string)x.Tag == "enemy")
+                {  //enemy hit op player of bij player langs//
+                    Canvas.SetTop(x, Canvas.GetTop(x) + EnemySpeed);
+
+                    if (Canvas.GetTop(x) > 750)
+                    {
+                        ItemRemover.Add(x);
+                        Damage += 10;
+                    }
+                    Rect EnemyHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if (PlayerHitBox.IntersectsWith(EnemyHitBox))
+                    {
+                        ItemRemover.Add(x);
+                        Damage += 5;
+                    }
+                }
+
+            }
+
+            foreach (Rectangle i in ItemRemover)
+            {
+                MyCanvas.Children.Remove(i);
+            }
+            //moeilijheidsgraad en Game Over//
+            /*
+                if (Score > 10)
+            {
+                Limit = 20;
+                EnemySpeed = 20;
+            }
+            */
+            
+            if (Score > 40)
+            {
+                Limit = 20;
+                EnemySpeed = 11;
+            }
+            if(Score > 100)
+            {
+                Limit = 20;
+                EnemySpeed = 12;
+            }
+            
+            if (Damage > 99)
+            {
+                Gametimer.Stop();
+                Damagetext.Content = "damage: 100";
+                Damagetext.Foreground = Brushes.Red;
+                MessageBox.Show("Captain, You have Destroyed " + Score + " Alien ships!" + Environment.NewLine + "pres OK to play again!");
+
+                System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+                Application.Current.Shutdown();
+            }
+
+        }
+        //knop voor verplaatsing instellen//
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                MoveLeft = true; 
+            }
+            if (e.Key == Key.Right)
+            {
+                MoveRight = true;
+            }
+
+        }
+        //knop voor verplaatsing instellen + bullet spawnen//
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                MoveLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                MoveRight = false;
+            }
+            if (e.Key == Key.Space)
+            {
+                Rectangle NewBullet = new Rectangle
+                {
+                    Tag = "bullet",
+                    Height = 20,
+                    Width = 5,
+                    Fill = Brushes.White,
+                    Stroke = Brushes.Red,
+                };
+                Canvas.SetLeft(NewBullet, Canvas.GetLeft(Player) + Player.Width / 2);
+                Canvas.SetTop(NewBullet, Canvas.GetTop(Player) - NewBullet.Height);
+
+                MyCanvas.Children.Add(NewBullet);
+
+            }
+        }
+        //enemys generaten//
+        private void MakeEnimies()
+        {
+            ImageBrush EnemySprite = new ImageBrush();
+            EnemySpriteCounter = Ran.Next(1, 5);
+
+            switch (EnemySpriteCounter)
+            {
+                case 1:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/1.png"));
+                    break;
+                case 2:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/2.png"));
+                    break;
+                case 3:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/3.png"));
+                    break;
+                case 4:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/4.png"));
+                    break;
+                case 5:
+                    EnemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/5.png"));
+                    break;
+
+            }
+
+            Rectangle NewEnemy = new Rectangle
+            { 
+                Tag = "enemy",
+                Height = 50,
+                Width = 56,
+                Fill = EnemySprite,
+            };
+
+            Canvas.SetTop(NewEnemy, -100);
+            Canvas.SetLeft(NewEnemy, Ran.Next(30, 430));
+            MyCanvas.Children.Add(NewEnemy);
+        }
+       
+    }
+}
---
+++ voorbeelden game p1/space battle game/space battle game/hoofdmenu.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace space_battle_game
+{
+    /// <summary>
+    /// Interaction logic for hoofdmenu.xaml
+    /// </summary>
+    /* <Grid DataGridRow.Selected="1">
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition></ColumnDefinition>
+            <ColumnDefinition></ColumnDefinition>
+        </Grid.ColumnDefinitions>
+    </Grid>
+   */
+    public partial class hoofdmenu : Window
+    {
+        public hoofdmenu()
+        {
+            InitializeComponent();
+
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            Hoofdmenu.Background = bg;
+        }
+
+        private void Start1Player_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Start2Player_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Highscore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+}
