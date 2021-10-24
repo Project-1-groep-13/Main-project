30fa951fa9ade9e77e498d41bc22fa90456f8ef0
Achmet-h <91320230+Achmet-h@users.noreply.github.com>
2021-10-25 01:11:25 +0200

    bug fixes

---
+++ project p1/PlayAgainMenu.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_p1
+{
+    /// <summary>
+    /// Interaction logic for PlayAgainMenu.xaml
+    /// </summary>
+    public partial class PlayAgainMenu : Window
+    {
+        public PlayAgainMenu()
+        {
+            InitializeComponent();
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            this.Background = bg;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            EasyMode easyMode = new EasyMode();
+            easyMode.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //high score menu
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow hoofdmenu = new MainWindow();
+            hoofdmenu.Visibility = Visibility.Visible;
+        }
+    }
+}
