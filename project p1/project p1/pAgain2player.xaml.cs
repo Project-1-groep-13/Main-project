854a849f0ef13dda6ace6bd98412078503fb177a
Chrispasma <chris.pasma@student.nhlstenden.com>
2021-12-07 11:40:05 +0100

    game2playerplayagain has comments

--- project p1/project p1/pAgain2player.xaml.cs
+++ project p1/project p1/pAgain2player.xaml.cs
@@ -20,34 +20,34 @@ namespace project_p1
         public pAgain2player()
         {
             InitializeComponent();
-            ImageBrush bg = new ImageBrush();
-            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            ImageBrush bg = new ImageBrush(); //create new imagebrush bg 
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png")); //getting image from folder for bg
             bg.TileMode = TileMode.Tile;
             bg.Viewport = new Rect(0, 0, 0.15, 0.15);
             bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
-            this.Background = bg;
+            this.Background = bg; //set background as bg 
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            this.Close();
-            HighScore highScore = new HighScore();
-            highScore.Visibility = Visibility.Visible;
+            this.Close(); //close window 
+            HighScore highScore = new HighScore(); //open highscore window 
+            highScore.Visibility = Visibility.Visible; //set highscore window as visible 
 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
-            MainWindow main = new MainWindow();
-            main.Visibility = Visibility.Visible;
+            this.Close(); //close window 
+            MainWindow main = new MainWindow(); //open main window 
+            main.Visibility = Visibility.Visible; //set main window as visible 
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            this.Close();
-            PlayerData2 playerData2 = new PlayerData2();
-            playerData2.Visibility = Visibility.Visible;
+            this.Close(); //close window 
+            PlayerData2 playerData2 = new PlayerData2(); //open playerdata2 window 
+            playerData2.Visibility = Visibility.Visible; //set playerdata2 window as visible 
         }
     }
 }
