6918313c2eacabc9cd646e76f2747ce9901aebc0
Chrispasma <chris.pasma@student.nhlstenden.com>
2021-12-07 11:11:12 +0100

    hoofdmenu code commentaar

--- project p1/project p1/MainWindow.xaml.cs
+++ project p1/project p1/MainWindow.xaml.cs
@@ -22,46 +22,46 @@ namespace project_p1
         {
             InitializeComponent();
 
-            ImageBrush bg = new ImageBrush();
-            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/backgroundfoto.jpg"));
-            Hoofdmenu.Background = bg;
-            Grid grid = new Grid();
-            grid.Background = bg;
+            ImageBrush bg = new ImageBrush(); //creating imagebrush for background
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/backgroundfoto.jpg")); //getting image from file 
+            Hoofdmenu.Background = bg; //set background for Hoofdmenu as bg image 
+            Grid grid = new Grid(); //make new grid 
+            grid.Background = bg; //set background grid as bg image 
 
         }
 
         private void Start1Player_Click(object sender, RoutedEventArgs e)
         {
-            PlayerData playerData = new PlayerData();
-            playerData.Visibility = Visibility.Visible;
+            PlayerData playerData = new PlayerData(); //create new 1player window 
+            playerData.Visibility = Visibility.Visible; //set 1player as visible
 
         }
 
         private void Start2Player_Click(object sender, RoutedEventArgs e)
         {
-            PlayerData2 playerData2 = new PlayerData2();
-            playerData2.Visibility = Visibility.Visible;
+            PlayerData2 playerData2 = new PlayerData2(); //create new 2player window
+            playerData2.Visibility = Visibility.Visible; //set 2player as visible 
  
         }
 
         private void Highscore_Click(object sender, RoutedEventArgs e)
         {
-            HighScore highScore = new HighScore();
-            highScore.Visibility = Visibility.Visible;
-            this.Close();
+            HighScore highScore = new HighScore(); //create new highscore window
+            highScore.Visibility = Visibility.Visible; //set highscore as visible 
+            this.Close(); //close hoofdmenu 
 
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            Application.Current.Shutdown(); //exit hoofdmenu and shutdown game 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            EasyMode easyMode = new EasyMode();
-            this.Close();
-            easyMode.Visibility = Visibility.Visible;
+            EasyMode easyMode = new EasyMode(); //create new easymode window
+            this.Close(); //close hoofdmenu 
+            easyMode.Visibility = Visibility.Visible; //set easymode as visible
         }
     }
 }
